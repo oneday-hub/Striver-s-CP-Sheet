32ba9b3077d0912177a6726790a64aa5e13d8eed
Nagendra Gupta <guptanagendra107@gmail.com>
2026-08-23 23:04:12 +0530

    problem 40 and 41

---
+++ Implementation/problem40.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+void solve(){
+    ll n, x, y;
+    cin >> n >> x >> y;
+
+    
+}
+int main() {
+    fast_io;
+    solve();
+    
+    return 0;
+}
---
+++ Implementation/problem41.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() {
+    fast_io;
+    
+    
+    return 0;
+}
