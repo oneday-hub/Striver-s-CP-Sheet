38e5c59fb2ae780f5f220d9aa473cbf81c76f1e6
Nagendra Gupta <guptanagendra107@gmail.com>
2026-08-24 10:42:07 +0530

    solution of problem 40

--- Implementation/problem40.cpp
+++ Implementation/problem40.cpp
@@ -1,12 +1,43 @@
+// question : A. City Day
+
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
 #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
 
+
 void solve(){
-    ll n, x, y;
+    int n, x, y;
     cin >> n >> x >> y;
 
+    vector<int> a(n);
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    for(int d = 0; d < n; d++){
+        bool flag = true;
+
+        // left
+        for(int j = max(0, d - x);j < d; j++){
+            if(a[d] > a[j]){
+                flag = false;
+                break;
+            }
+        }
+        // right
+        if(flag){
+            for(int j = d + 1; j <= min(n - 1, d + y); j++){
+                if(a[d] > a[j]){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        if(flag){
+            cout << d + 1 << endl;
+            break;
+        }
+    }
     
 }
 int main() {
--- Implementation/problem41.cpp
+++ Implementation/problem41.cpp
@@ -1,11 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
 
+void solve(){
+    int n, x, y
+}
 int main() {
     fast_io;
-    
+    solve();
     
     return 0;
 }
