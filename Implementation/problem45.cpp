a5b89298fb1c51d63b7bd7bf4974652e5c7d11c3
Nagendra Gupta <guptanagendra107@gmail.com>
2026-09-01 14:39:16 +0530

    solution of suffle hashing

--- Implementation/problem45.cpp
+++ Implementation/problem45.cpp
@@ -1,18 +1,48 @@
-// A. Shuffle Hashing
-
 #include <bits/stdc++.h>
 using namespace std;
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr);
-void solve(){
-    
+
+void solve() {
+    string p, h;
+    cin >> p;
+    cin >> h;
+
+    unordered_map<char, int> mp;
+
+    for (char ch : p) {
+        mp[ch]++;
+    }
+
+    int k = p.size();
+    int m = h.size();
+
+    for (int i = 0; i + k <= m; i++) {
+
+        unordered_map<char, int> temp;
+
+         for (int j = i; j < i + k; j++) {
+            temp[h[j]]++;
+        }
+
+        if (mp == temp) {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+
+    cout << "NO" << endl;
 }
+
 int main() {
     fast_io;
-    int t; 
-    cin >> t; 
-    while(t--){
+
+    int t;
+    cin >> t;
+
+    while (t--) {
         solve();
     }
+
     return 0;
 }
