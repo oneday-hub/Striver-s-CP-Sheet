f0ee24a76faaee46129cd6ff8c0c19b19c0e687c
Nagendra Gupta <guptanagendra107@gmail.com>
2026-09-01 15:42:02 +0530

    solution of A. Middle of the Contest

--- Implementation/problem45.cpp
+++ Implementation/problem45.cpp
@@ -1,3 +1,5 @@
+// A. Shuffle Hashing
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- Implementation/problem46.cpp
+++ Implementation/problem46.cpp
@@ -5,6 +5,26 @@ using namespace std;
 
 int main() {
     fast_io;
-    
+    int h1, m1, h2, m2;
+    char c;
+    cin >> h1 >> c >> m1;
+    cin >> h2 >> c >> m2;
+
+    int t1 = h1 * 60 + m1;
+    int t2 = h2 * 60 + m2;
+
+    int tt = (t1 + t2) /2;
+    int h3 = tt / 60;
+    int m3 = tt % 60;
+
+    if(h3 < 10){
+        cout << "0";
+    }
+    cout << h3 << ":";
+
+    if(m3 < 10){
+        cout << "0";
+    }
+    cout << m3;
     return 0;
 }
